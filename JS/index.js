d061412aeeab2fcb5e87a83e36d4f401b3966353
FarhaanUDM <misurfoodeen@student.udm.ac.mu>
2024-02-28 23:28:09 +0400

    complete project

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+/* =============== Typing Animation================= */
+var typed = new Typed('.typing', {
+  strings: [
+    'Entry-level Software Engineer',
+    'Web Deisgner',
+    'Web Developer',
+    'Graphic Designer',
+    'YouTuber',
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
